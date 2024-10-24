1cb42629cb1a3fd3a7a6a864329049edafc8b735
Eddie Maceda <eddiemaceda11@gmail.com>
2024-10-23 21:51:40 -0700

    Set up post, delete, and get single id endpoints

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+app.use(express.json());
+
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -28,16 +30,51 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(404).json({ error: "name must be unique" });
+  }
+
+  for (let person of persons) {
+    if (person.name == body.name) {
+      return res.status(404).end({ error: "name must be unique" });
+    }
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 10000),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+  res.json(persons);
+});
+
 app.get("/info", (req, res) => {
   const content = `<p>Phonebook has info for ${persons.length} people</p>
   <p>${Date()}</p>`;
   res.send(content);
 });
 
-app,
-  get("/api/persons/:id", (req, res) => {
-    const id = req.params.id;
-  });
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const personToSend = persons.find((person) => person.id === id);
+  if (personToSend) {
+    res.json(personToSend);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
 
 app.listen(3001, (req, res) => {
   console.log("running");
