33a57ff68970747aa0fa2a7a61bff16fdbb987d2
Eddie Maceda <eddiemaceda11@gmail.com>
2024-10-25 15:40:45 -0700

    Update Port number

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ morgan.token("type", function (req, res) {
   return JSON.stringify(req.body);
 });
 
-app.use(cors());
 app.use(express.static("dist"));
+app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :type"));
 
@@ -85,7 +85,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, (req, res) => {
   console.log(`Server running on port ${PORT}`);
 });
