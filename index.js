7bc67e9cb2a15cb3615ba16b30994577e8fc58aa
Eddie Maceda <eddiemaceda11@gmail.com>
2024-10-25 10:51:38 -0700

    Configure morgan middleware to log request body along with request method

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+morgan.token("type", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
 app.use(express.json());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :type"));
 
 let persons = [
   {
